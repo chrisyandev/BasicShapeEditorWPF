f2067edc76bc7d68cbb0921c5b2944be745a34bc
chrisyandev <czyanhome@hotmail.com>
2023-03-25 00:49:27 -0700

    pick and spawn triangle

--- HierarchyTreeWPF/ViewModels/MainWindowViewModel.cs
+++ HierarchyTreeWPF/ViewModels/MainWindowViewModel.cs
@@ -13,6 +13,7 @@ using HierarchyTreeWPF.Commands;
 using System.Windows.Media;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace HierarchyTreeWPF.ViewModels
 {
@@ -78,8 +79,8 @@ namespace HierarchyTreeWPF.ViewModels
                 {
                     Id = 1,
                     Shape = newRectangle,
-                    X = Mouse.GetPosition(canvas).X,
-                    Y = Mouse.GetPosition(canvas).Y
+                    X = Mouse.GetPosition(canvas).X - (newRectangle.Width / 2),
+                    Y = Mouse.GetPosition(canvas).Y - (newRectangle.Height / 2)
                 };
 
                 CanvasItems.Add(new RectangleViewModel(newCanvasItem));
@@ -97,11 +98,37 @@ namespace HierarchyTreeWPF.ViewModels
                 {
                     Id = 1,
                     Shape = newEllipse,
-                    X = Mouse.GetPosition(canvas).X,
-                    Y = Mouse.GetPosition(canvas).Y
+                    X = Mouse.GetPosition(canvas).X - (newEllipse.Width / 2),
+                    Y = Mouse.GetPosition(canvas).Y - (newEllipse.Height / 2)
                 };
 
                 CanvasItems.Add(new EllipseViewModel(newCanvasItem));
+            }
+            else if (ShapeToAdd == "triangle")
+            {
+                Polygon newTriangle = new Polygon
+                {
+                    Fill = Brushes.Green,
+                    Points = new PointCollection
+                    {
+                        new Point(0, 50),
+                        new Point(25, 0),
+                        new Point(50, 50)
+                    }
+                };
+
+                double centroidX = (newTriangle.Points[0].X + newTriangle.Points[1].X + newTriangle.Points[2].X) / 3;
+                double centroidY = (newTriangle.Points[0].Y + newTriangle.Points[1].Y + newTriangle.Points[2].Y) / 3;
+
+                CanvasItem newCanvasItem = new CanvasItem
+                {
+                    Id = 1,
+                    Shape = newTriangle,
+                    X = Mouse.GetPosition(canvas).X - centroidX,
+                    Y = Mouse.GetPosition(canvas).Y - centroidY
+                };
+
+                CanvasItems.Add(new TriangleViewModel(newCanvasItem));
             }
         }
     }
---
+++ HierarchyTreeWPF/ViewModels/TriangleViewModel.cs
@@ -0,0 +1,148 @@
+﻿using HierarchyTreeWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace HierarchyTreeWPF.ViewModels
+{
+    public class TriangleViewModel : ShapeViewModelBase
+    {
+        private CanvasItem _canvasItem;
+        private Polygon _triangle;
+
+        public TriangleViewModel(CanvasItem canvasItem)
+        {
+            _canvasItem = canvasItem;
+            _triangle = _canvasItem.Shape as Polygon;
+        }
+
+        /*        public double Width
+                {
+                    get
+                    {
+                        List<double> pointsXAxis = _triangle.Points.Select(p => p.X).ToList();
+                        return pointsXAxis.Max() - pointsXAxis.Min();
+                    }
+                    set
+                    {
+                        List<double> pointsXAxis = _triangle.Points.Select(p => p.X).ToList();
+                        PointCollection newPoints = new PointCollection();
+
+                        foreach (Point point in _triangle.Points)
+                        {
+                            // leftmost point
+                            if (point.X == pointsXAxis.Min())
+                            {
+                                newPoints.Add(new Point(point.X - (value / 2), point.Y));
+                            }
+                            // rightmost point
+                            else if (point.X == pointsXAxis.Max())
+                            {
+                                newPoints.Add(new Point(point.X + (value / 2), point.Y));
+                            }
+                            // inbetween point
+                            else
+                            {
+                                newPoints.Add(new Point(point.X, point.Y));
+                            }
+                        }
+
+                        _triangle.Points = newPoints;
+                        OnPropertyChanged();
+
+                        Debug.WriteLine(Width);
+                    }
+                }
+
+                public double Height
+                {
+                    get
+                    {
+                        List<double> pointsYAxis = _triangle.Points.Select(p => p.Y).ToList();
+                        return pointsYAxis.Max() - pointsYAxis.Min();
+                    }
+                    set
+                    {
+                        List<double> pointsYAxis = _triangle.Points.Select(p => p.Y).ToList();
+                        PointCollection newPoints = new PointCollection();
+
+                        foreach (Point point in _triangle.Points)
+                        {
+                            // bottommost point
+                            if (point.X == pointsYAxis.Min())
+                            {
+                                newPoints.Add(new Point(point.X, point.Y - (value / 2)));
+                            }
+                            // topmost point
+                            else if (point.X == pointsYAxis.Max())
+                            {
+                                newPoints.Add(new Point(point.X, point.Y + (value / 2)));
+                            }
+                            // inbetween point
+                            else
+                            {
+                                newPoints.Add(new Point(point.X, point.Y));
+                            }
+                        }
+
+                        _triangle.Points = newPoints;
+                        OnPropertyChanged();
+
+                        Debug.WriteLine(Height);
+                    }
+                }*/
+
+        public PointCollection Points
+        {
+            get
+            {
+                return _triangle.Points;
+            }
+        }
+
+        public Brush Fill
+        {
+            get { return _triangle.Fill; }
+            set
+            {
+                if (value == _triangle.Fill) { return; }
+                _triangle.Fill = value;
+                OnPropertyChanged();
+
+                Debug.WriteLine(_triangle.Fill);
+            }
+        }
+
+        public double X
+        {
+            get { return _canvasItem.X; }
+            set
+            {
+                if (value == _canvasItem.X) { return; }
+                _canvasItem.X = value;
+                OnPropertyChanged();
+
+                Debug.WriteLine(_canvasItem.X);
+            }
+        }
+
+        public double Y
+        {
+            get { return _canvasItem.Y; }
+            set
+            {
+                if (value == _canvasItem.Y) { return; }
+                _canvasItem.Y = value;
+                OnPropertyChanged();
+
+                Debug.WriteLine(_canvasItem.Y);
+            }
+        }
+    }
+}
