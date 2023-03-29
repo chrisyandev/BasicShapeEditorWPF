75a59f741a21c7eeb9a72613fe3b216abfc22c20
chrisyandev <czyanhome@hotmail.com>
2023-03-29 13:10:23 -0700

    refactor: move all shape resizing logic into ShapeExtensions (canvas boundary still needs to be applied to triangle)

--- HierarchyTreeAndCanvasWPF/Adorners/ResizeAdorner.cs
+++ HierarchyTreeAndCanvasWPF/Adorners/ResizeAdorner.cs
@@ -48,121 +48,20 @@ namespace HierarchyTreeAndCanvasWPF.Adorners
 
         private void Thumb1_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            FrameworkElement element = (FrameworkElement)AdornedElement;
-
-            if (element is Polygon)
+            if (AdornedElement is Shape shape)
             {
-                element.ShiftLeftSide(e.HorizontalChange);
-                element.ShiftTopSide(e.VerticalChange);
-                return;
+                shape.ShiftLeftSide(e.HorizontalChange, 0);
+                shape.ShiftTopSide(e.VerticalChange, 0);
             }
-
-            double left = Canvas.GetLeft(element);
-            double top = Canvas.GetTop(element);
-            double right = left + element.Width;
-            double bottom = top + element.Height;
-
-            double newWidth, newLeft, newHeight, newTop;
-
-            // shape right edge is the limit
-            if (left + e.HorizontalChange > right)
-            {
-                newWidth = 0;
-                newLeft = right;
-            }
-            else
-            {
-                newWidth = element.Width - e.HorizontalChange;
-                newLeft = left + e.HorizontalChange;
-            }
-
-            // shape bottom edge is the limit
-            if (top + e.VerticalChange > bottom)
-            {
-                newHeight = 0;
-                newTop = bottom;
-            }
-            else
-            {
-                newHeight = element.Height - e.VerticalChange;
-                newTop = top + e.VerticalChange;
-            }
-
-            // canvas left side is the limit
-            if (newLeft < 0)
-            {
-                double unitsOverLimit = 0 - newLeft;
-                newWidth -= unitsOverLimit;
-                newLeft += unitsOverLimit;
-            }
-
-            // canvas top side is the limit
-            if (newTop < 0)
-            {
-                double unitsOverLimit = 0 - newTop;
-                newHeight -= unitsOverLimit;
-                newTop += unitsOverLimit;
-            }
-
-            element.Width = newWidth;
-            Canvas.SetLeft(element, newLeft);
-            element.Height = newHeight;
-            Canvas.SetTop(element, newTop);
         }
 
         private void Thumb2_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            FrameworkElement element = (FrameworkElement)AdornedElement;
-
-            if (element is Polygon)
+            if (AdornedElement is Shape shape)
             {
-                element.ShiftRightSide(e.HorizontalChange);
-                element.ShiftBottomSide(e.VerticalChange);
-                //element.Resize(e.HorizontalChange, e.VerticalChange);
-                return;
+                shape.ShiftRightSide(e.HorizontalChange, _canvas.ActualWidth);
+                shape.ShiftBottomSide(e.VerticalChange, _canvas.ActualHeight);
             }
-
-            double left = Canvas.GetLeft(element);
-            double top = Canvas.GetTop(element);
-
-            double newWidth, newHeight;
-
-            // shape left edge is the limit
-            if (element.Width + e.HorizontalChange < 0)
-            {
-                newWidth = 0;
-            }
-            else
-            {
-                newWidth = element.Width + e.HorizontalChange;
-            }
-
-            // shape top edge is the limit
-            if (element.Height + e.VerticalChange < 0)
-            {
-                newHeight = 0;
-            }
-            else
-            {
-                newHeight = element.Height + e.VerticalChange;
-            }
-
-            // canvas right side is the limit
-            if (left + newWidth > _canvas.ActualWidth)
-            {
-                double unitsOverLimit = (left + newWidth) - _canvas.ActualWidth;
-                newWidth -= unitsOverLimit;
-            }
-
-            // canvas bottom side is the limit
-            if (top + newHeight > _canvas.ActualHeight)
-            {
-                double unitsOverLimit = (top + newHeight) - _canvas.ActualHeight;
-                newHeight -= unitsOverLimit;
-            }
-
-            element.Width = newWidth;
-            element.Height = newHeight;
         }
 
         protected override Visual GetVisualChild(int index)
---
+++ HierarchyTreeAndCanvasWPF/Extensions/ShapeExtensions.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Shapes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Printing;
+
+namespace HierarchyTreeAndCanvasWPF.Extensions
+{
+    public static class ShapeExtensions
+    {
+        public static void ShiftLeftSide(this Shape shape, double units, double leftBoundary)
+        {
+            if (shape is Rectangle rectangle)
+            {
+                ShiftLeftSideShapeWithWidthAndHeight(rectangle, units, leftBoundary);
+            }
+            else if (shape is Ellipse ellipse)
+            {
+                ShiftLeftSideShapeWithWidthAndHeight(ellipse, units, leftBoundary);
+            }
+            else if (shape is Polygon triangle)
+            {
+                ShiftLeftSideTriangle(triangle, units);
+            }
+        }
+
+        public static void ShiftTopSide(this Shape shape, double units, double topBoundary)
+        {
+            if (shape is Rectangle rectangle)
+            {
+                ShiftTopSideShapeWithWidthAndHeight(rectangle, units, topBoundary);
+            }
+            else if (shape is Ellipse ellipse)
+            {
+                ShiftTopSideShapeWithWidthAndHeight(ellipse, units, topBoundary);
+            }
+            else if (shape is Polygon triangle)
+            {
+                ShiftTopSideTriangle(triangle, units);
+            }
+        }
+
+        public static void ShiftRightSide(this Shape shape, double units, double rightBoundary)
+        {
+            if (shape is Rectangle rectangle)
+            {
+                ShiftRightSideShapeWithWidthAndHeight(rectangle, units, rightBoundary);
+            }
+            else if (shape is Ellipse ellipse)
+            {
+                ShiftRightSideShapeWithWidthAndHeight(ellipse, units, rightBoundary);
+            }
+            else if (shape is Polygon triangle)
+            {
+                ShiftRightSideTriangle(triangle, units);
+            }
+        }
+
+        public static void ShiftBottomSide(this Shape shape, double units, double bottomBoundary)
+        {
+            if (shape is Rectangle rectangle)
+            {
+                ShiftBottomSideShapeWithWidthAndHeight(rectangle, units, bottomBoundary);
+            }
+            else if (shape is Ellipse ellipse)
+            {
+                ShiftBottomSideShapeWithWidthAndHeight(ellipse, units, bottomBoundary);
+            }
+            else if (shape is Polygon triangle)
+            {
+                ShiftBottomSideTriangle(triangle, units);
+            }
+        }
+
+        private static void ShiftLeftSideShapeWithWidthAndHeight(Shape shape, double units, double leftBoundary)
+        {
+            double left = Canvas.GetLeft(shape);
+            double right = left + shape.Width;
+
+            double newWidth, newLeft;
+
+            // shape right edge is the limit
+            if (left + units > right)
+            {
+                newWidth = 0;
+                newLeft = right;
+            }
+            else
+            {
+                newWidth = shape.Width - units;
+                newLeft = left + units;
+            }
+
+            // canvas left side is the limit
+            if (newLeft < leftBoundary)
+            {
+                double unitsOverLimit = leftBoundary - newLeft;
+                newWidth -= unitsOverLimit;
+                newLeft += unitsOverLimit;
+            }
+
+            shape.Width = newWidth;
+            Canvas.SetLeft(shape, newLeft);
+        }
+
+        private static void ShiftTopSideShapeWithWidthAndHeight(Shape shape, double units, double topBoundary)
+        {
+            double top = Canvas.GetTop(shape);
+            double bottom = top + shape.Height;
+
+            double newHeight, newTop;
+
+            // shape bottom edge is the limit
+            if (top + units > bottom)
+            {
+                newHeight = 0;
+                newTop = bottom;
+            }
+            else
+            {
+                newHeight = shape.Height - units;
+                newTop = top + units;
+            }
+
+            // canvas top side is the limit
+            if (newTop < topBoundary)
+            {
+                double unitsOverLimit = topBoundary - newTop;
+                newHeight -= unitsOverLimit;
+                newTop += unitsOverLimit;
+            }
+
+            shape.Height = newHeight;
+            Canvas.SetTop(shape, newTop);
+        }
+
+        private static void ShiftRightSideShapeWithWidthAndHeight(Shape shape, double units, double rightBoundary)
+        {
+            double left = Canvas.GetLeft(shape);
+
+            double newWidth;
+
+            // shape left edge is the limit
+            if (shape.Width + units < 0)
+            {
+                newWidth = 0;
+            }
+            else
+            {
+                newWidth = shape.Width + units;
+            }
+
+            // canvas right side is the limit
+            if (left + newWidth > rightBoundary)
+            {
+                double unitsOverLimit = (left + newWidth) - rightBoundary;
+                newWidth -= unitsOverLimit;
+            }
+
+            shape.Width = newWidth;
+        }
+
+        private static void ShiftBottomSideShapeWithWidthAndHeight(Shape shape, double units, double bottomBoundary)
+        {
+            double top = Canvas.GetTop(shape);
+
+            double newHeight;
+
+            // shape top edge is the limit
+            if (shape.Height + units < 0)
+            {
+                newHeight = 0;
+            }
+            else
+            {
+                newHeight = shape.Height + units;
+            }
+
+            // canvas bottom side is the limit
+            if (top + newHeight > bottomBoundary)
+            {
+                double unitsOverLimit = (top + newHeight) - bottomBoundary;
+                newHeight -= unitsOverLimit;
+            }
+
+            shape.Height = newHeight;
+        }
+
+        private static void ShiftLeftSideTriangle(Polygon triangle, double units)
+        {
+            List<double> pointsXAxis = triangle.Points.Select(p => p.X).ToList();
+            PointCollection newPoints = new PointCollection();
+
+            Point leftPoint;
+            Point topPoint;
+            Point rightPoint;
+
+            foreach (Point point in triangle.Points)
+            {
+                if (point.X == pointsXAxis.Min())
+                {
+                    leftPoint = point;
+                }
+                else if (point.X > pointsXAxis.Min() && point.X < pointsXAxis.Max())
+                {
+                    topPoint = point;
+                }
+                else if (point.X == pointsXAxis.Max())
+                {
+                    rightPoint = point;
+                }
+            }
+
+            Point newLeftPoint = leftPoint;
+            Point newTopPoint = topPoint;
+            Point newRightPoint = rightPoint;
+
+            // triangle right side is limit
+            if (leftPoint.X + units < pointsXAxis.Max())
+            {
+                // takes care of moving left point
+                Canvas.SetLeft(triangle, Canvas.GetLeft(triangle) + units);
+                // adjust right point based on how much left point moves
+                newRightPoint = new Point(rightPoint.X - units, rightPoint.Y);
+            }
+            // min width is 1 unit
+            else
+            {
+                Canvas.SetLeft(triangle, Canvas.GetLeft(triangle) + topPoint.X - 0.5);
+                newRightPoint = new Point(topPoint.X + 0.5, rightPoint.Y);
+            }
+
+            // accurately calculates inbetween point
+            newTopPoint = new Point((newRightPoint.X - newLeftPoint.X) / 2, topPoint.Y);
+
+            newPoints.Add(newLeftPoint);
+            newPoints.Add(newTopPoint);
+            newPoints.Add(newRightPoint);
+            triangle.Points = newPoints;
+        }
+
+        private static void ShiftTopSideTriangle(Polygon triangle, double units)
+        {
+            List<double> pointsXAxis = triangle.Points.Select(p => p.X).ToList();
+            PointCollection newPoints = new PointCollection();
+
+            Point leftPoint;
+            Point topPoint;
+            Point rightPoint;
+
+            foreach (Point point in triangle.Points)
+            {
+                if (point.X == pointsXAxis.Min())
+                {
+                    leftPoint = point;
+                }
+                else if (point.X > pointsXAxis.Min() && point.X < pointsXAxis.Max())
+                {
+                    topPoint = point;
+                }
+                else if (point.X == pointsXAxis.Max())
+                {
+                    rightPoint = point;
+                }
+            }
+
+            Point newLeftPoint = leftPoint;
+            Point newTopPoint = topPoint;
+            Point newRightPoint = rightPoint;
+            double minHeight = 1;
+            double height = leftPoint.Y - topPoint.Y;
+            double newTop = Canvas.GetTop(triangle) + units;
+            double newHeight = height - units;
+
+            // triangle bottom side is limit
+            if (newHeight < minHeight)
+            {
+                newTop = Canvas.GetTop(triangle) + height - minHeight;
+                newHeight = minHeight;
+            }
+
+            Canvas.SetTop(triangle, newTop);
+            newLeftPoint = new Point(leftPoint.X, newHeight);
+            newRightPoint = new Point(rightPoint.X, newHeight);
+
+            newPoints.Add(newLeftPoint);
+            newPoints.Add(newTopPoint);
+            newPoints.Add(newRightPoint);
+            triangle.Points = newPoints;
+        }
+
+        private static void ShiftRightSideTriangle(Polygon triangle, double units)
+        {
+            List<double> pointsXAxis = triangle.Points.Select(p => p.X).ToList();
+            PointCollection newPoints = new PointCollection();
+
+            foreach (Point point in triangle.Points)
+            {
+                // leftmost point
+                if (point.X == pointsXAxis.Min())
+                {
+                    newPoints.Add(new Point(point.X, point.Y));
+                }
+                // rightmost point
+                else if (point.X == pointsXAxis.Max())
+                {
+                    // detect if right side will overlap with left, leave a 1 unit gap between left and right side
+                    if (pointsXAxis.Max() + units > pointsXAxis.Min())
+                    {
+                        newPoints.Add(new Point(pointsXAxis.Max() + units, point.Y));
+                    }
+                    else
+                    {
+                        newPoints.Add(new Point(pointsXAxis.Min() + 1, point.Y));
+                    }
+                }
+                // inbetween point
+                else if (point.X > pointsXAxis.Min() && point.X < pointsXAxis.Max())
+                {
+                    // detect if right side will overlap with left, leave a 0.5 unit gap between left -> mid and mid -> right
+                    if (pointsXAxis.Max() + units > pointsXAxis.Min())
+                    {
+                        newPoints.Add(new Point(point.X + (units / 2), point.Y));
+                    }
+                    else
+                    {
+                        newPoints.Add(new Point(pointsXAxis.Min() + 0.5, point.Y));
+                    }
+                }
+            }
+            triangle.Points = newPoints;
+        }
+
+        private static void ShiftBottomSideTriangle(Polygon triangle, double units)
+        {
+            List<double> pointsXAxis = triangle.Points.Select(p => p.X).ToList();
+            PointCollection newPoints = new PointCollection();
+
+            foreach (Point point in triangle.Points)
+            {
+                // leftmost point
+                if (point.X == pointsXAxis.Min())
+                {
+                    newPoints.Add(new Point(point.X, point.Y + units));
+                }
+                // rightmost point
+                else if (point.X == pointsXAxis.Max())
+                {
+                    newPoints.Add(new Point(point.X, point.Y + units));
+                }
+                // inbetween point
+                else
+                {
+                    newPoints.Add(new Point(point.X, point.Y));
+                }
+            }
+            triangle.Points = newPoints;
+        }
+    }
+}
